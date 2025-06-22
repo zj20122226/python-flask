4b0b21467e1cc362927f9fb5e0b7c64c2ad425ba
zj20122226 <130367027+zj20122226@users.noreply.github.com>
2025-06-23 04:06:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello Woeld"
+    return "Hello world!"
 
 def main():
-    app.run(port=int(os.environ.get('PORT', 80)))
+    app.run(port=int(os.environ.get('PORT', 3000)))
 
 if __name__ == "__main__":
     main()
