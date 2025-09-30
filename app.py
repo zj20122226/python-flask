a8e32fa3fb8c19260c3369f4a7abc4d7e153cca1
zj20122226 <130367027+zj20122226@users.noreply.github.com>
2025-09-30 16:41:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def test():
 def main():
     port = int(os.environ.get('PORT', 5000))
     print(f"App is Running at {port} port!")
-    app.run(host='0.0.0.0', port=port))
+    app.run(host='0.0.0.0', port=port)
 
 if __name__ == "__main__":
     main()
