3ac3594e98d40d5bc57dd56a6a1f1649776dc2da
zj20122226 <130367027+zj20122226@users.noreply.github.com>
2025-09-30 16:17:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,20 @@ app = Flask(__name__)
 def index():
     return "Hello world!"
 
+@app.route("/workers_wake_up")
+def workers_wake_up():
+    return "Hey!Wake Up!!"
+
+@app.route("/test")
+def test():
+    print("this is a test print")
+    app.logger.info("Request test success!")
+    return "this is a test print"
+
 def main():
-    app.run(port=int(os.environ.get('PORT', 3000)))
+    port = int(os.environ.get('PORT', 5000))
+    print(f"App is Running at {port} port!")
+    app.run(host='0.0.0.0', port=port))
 
 if __name__ == "__main__":
     main()
