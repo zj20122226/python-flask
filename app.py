4c5b575e5631b98d45efba049ef12ff8c2c6188b
zj20122226 <zj20122226@users.noreply.github.com>
2026-07-05 03:36:44 +0000

    deploy: 56b51dbec55b02ce6de234220cff6ebd40ba2051

---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+
+from flask import Flask, send_file
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello world!"
+
+@app.route("/workers_wake_up")
+def workers_wake_up():
+    return "Hey!Wake Up!!"
+
+@app.route("/test")
+def test():
+    print("this is a test print")
+    app.logger.info("Request test success!")
+    return "this is a test print"
+
+def main():
+    port = int(os.environ.get('PORT', 5000))
+    print(f"App is Running at {port} port!")
+    app.run(host='0.0.0.0', port=port)
+
+if __name__ == "__main__":
+    main()
